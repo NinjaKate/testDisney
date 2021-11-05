f7898b1d275457a0d21ba643738ca125ac866129
NinjaKate <38056303+NinjaKate@users.noreply.github.com>
2021-11-05 20:07:56 +0300

    paginator update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from "@angular/router";
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  characters: any[] = [];
+  paginator: any = {
+    pageSize: 10,
+    pageIndex: 0,
+    length: 0,
+  };
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
+
+  setPage(event: any) {
+    this.paginator.pageSize = event.pageSize;
+    this.paginator.pageIndex = event.pageIndex;
+  }
 
   ngOnInit(): void {
+    this.dataService.getRemoveData().subscribe((obj => {
+      this.characters = obj.data;
+      this.paginator.length = this.characters.length;
+      console.log(this.characters[0])
+    }))
   }
+
 
 }
