33065bf3d7194b4479d69cdd92f714bccc1638d9
NinjaKate <38056303+NinjaKate@users.noreply.github.com>
2021-11-07 04:19:48 +0300

    sidebar and cards update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {DatePipe} from "@angular/common";
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import {DatePipe} from "@angular/common";
     HttpClientModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule
   ],
   providers: [
     DatePipe,
